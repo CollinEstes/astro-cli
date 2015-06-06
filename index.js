57fb129e24437a6236098ba726ae1ad5ba98ee5e
Collin Estes <Collin.j.estes@gmail.com>
2015-06-06 18:54:03 -0500

    testing "astro mocha babel"
    But not working at all...

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 #! /usr/bin/env nodejs
 var spawn = require('child_process').spawn,
     astroCmd = process.argv[2],
+    arg1 = process.argv[3],
     cwd = process.cwd(),
-    cmd = spawn('docker', ['run',
-                              '-t',
-                              '-v',
-                              cwd+':/src/app',
-                              'mikefielden/astrokit:'+ astroCmd ]);
+    cmd;
+
+// astroCmd = astroCmd + ' "mocha --compilers:js:babel/register"';
+
+if (astroCmd === 'mocha' && arg1 === 'babel') {
+	cmd = spawn('docker', ['run',
+                            '-t',
+                            '-v',
+                            cwd+':/src/app',
+                            'mikefielden/astrokit:'+ astroCmd]);
+} else {
+	cmd = spawn('docker', ['run',
+												'-t',
+												'-v',
+												cwd+':/src/app',
+												'mikefielden/astrokit:'+ astroCmd]);
+
+}
 
 cmd.stdout.on('data', function (data) {
   console.log(data.toString());
