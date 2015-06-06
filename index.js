0db1af18b12221c8f16bd183e9227c5be479689d
Mike Fielden <fielden.mike@gmail.com>
2015-06-06 12:44:22 -0500

    Deleting old code

--- index.js
+++ index.js
@@ -19,22 +19,3 @@ cmd.stderr.on('data', function (data) {
 cmd.on('exit', function (code) {
   console.log('astro %s completed with code %s', astroCmd, code);
 });
-
-
-
-//var args = process.argv;
-//
-//// first two args are bash and bin/astro
-//var command = args[2];
-//
-//
-//
-//  var exec = require('child_process').exec;
-//
-//  exec('docker run -t -v /$(pwd):/src/app mikefielden/astrokit:jshint', function (error, stdout, stderr) {
-//    if (error) {
-//      console.error(error, stderr);
-//    }
-//
-//    console.log(stdout);
-//  });
