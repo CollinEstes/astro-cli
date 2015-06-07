ca4edaf8ba1786232e88638ea84eedf6a63c24bf
Collin Estes <Collin.j.estes@gmail.com>
2015-06-06 23:22:08 -0500

    added watch

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-#! /usr/bin/env nodejs
+#! /usr/bin/env node
 var spawn = require('child_process').spawn,
-    astroCmd = process.argv[2],
-    arg1 = process.argv[3],
-    cwd = process.cwd(),
-    cmd;
+		argv = process.argv,
+		astroCmd = argv[2],
+		astroArgs = argv.splice
+		cwd = process.cwd(),
+		command = 'docker',
+		args = ['run',
+						'-t',
+						'-v',
+						cwd+':/src/app'];
 
-// astroCmd = astroCmd + ' "mocha --compilers:js:babel/register"';
 
-if (astroCmd === 'mocha' && arg1 === 'babel') {
-	cmd = spawn('docker', ['run',
-                            '-t',
-                            '-v',
-                            cwd+':/src/app',
-                            'mikefielden/astrokit:'+ astroCmd]);
-} else {
-	cmd = spawn('docker', ['run',
-												'-t',
-												'-v',
-												cwd+':/src/app',
-												'mikefielden/astrokit:'+ astroCmd]);
-
+// handle additional arguments
+if (astroArgs.length !== 0) {
+	// THIS IS WHERE WE WOULD ADD ADDITIONAL ARGUEMENTS TO ARGS
 }
 
-cmd.stdout.on('data', function (data) {
-  console.log(data.toString());
-});
 
-cmd.stderr.on('data', function (data) {
-  console.log('stderr', data.toString());
-});
+// check for 'watch' command
+if (astroCmd === 'watch') {
+	// default for watch is mocha
+	args.push('mikefielden/astrokit:mocha');
+	watch(command, args);
+} else {
 
-cmd.on('exit', function (code) {
-  console.log('astro %s completed with code %s', astroCmd, code);
-});
+	// add selected image to args
+	args.push('mikefielden/astrokit:'+ astroCmd);
+	runCommand(command, args);
+}
+
+
+function runCommand (command, args) {
+	// set up
+	var dockerRunCmd = spawn(command, args);
+
+	dockerRunCmd.stdout.on('data', function (data) {
+		console.log(data.toString());
+	});
+
+	dockerRunCmd.stderr.on('data', function (data) {
+		console.log('stderr', data.toString());
+	});
+
+	dockerRunCmd.on('exit', function (code) {
+		console.log('astro %s completed with code %s', astroCmd, code);
+	});
+};
+
+
+
+function watch (command, args) {
+	var watch = require('watch');
+
+	watch.watchTree(cwd, function (f, curr, prev) {
+		console.log('WATCH FOUND');
+    runCommand(command, args);
+  });
+
+}
